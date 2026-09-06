10d08e5edc0f9dd73ff33081e2070e6a087d74f2
aryannsainiii-hub <aryannsainiii@gmail.com>
2026-09-07 00:16:44 +0530

    changed commponets of main page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import TransportCategoryCards from "../components/home/TransportCategoryCards";
 import SmartJourneySection from "../components/home/SmartJourneySection";
 import PopularDestinations from "../components/home/PopularDestinations";
 import CTABanner from "../components/home/CTABanner";
-
+//home page with hero, search, transport categories, smart journey, popular destinations and cta banner
 export default function HomePage() {
   return (
     <>
